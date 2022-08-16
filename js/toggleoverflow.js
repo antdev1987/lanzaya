bf01cb79fc964de86136c32fbd149621179cf97c
antdev <antdev1987@gmail.com>
2022-08-16 00:35:24 -0600

    overflow js deleted

--- js/toggleoverflow.js
+++
@@ -1,7 +0,0 @@
-const html = document.querySelector('html');
-
-const hamburguer = document.querySelector('.hamburguer');
-
-hamburguer.addEventListener('click', () => {
-    html.classList.toggle('overflow-hidden')
-})
